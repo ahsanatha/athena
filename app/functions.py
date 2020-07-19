28d9af125919104855ef91f93a2dff1e109eb3d7
ahsanatha <ahsan.athallah@gmail.com>
2020-07-19 13:42:17 +0700

    apa

--- app/functions.py
+++ app/functions.py
@@ -16,6 +16,7 @@ def getNIM(nim):
 
 def getSummary(nim):
     mks = getNIM(nim)
+    print(mks)
     # mks = [{'id': 2, 'timestamp': '19/07/2020 2:14:28', 'nim': 1301174068, 'kalkulusIb': 3, 'logikaMatematikaA': 4, 'dasarAlgoritmaDanPemrograman': 1, 'matematikaDiskritA': 3, 'kalkulusIib': 4, 'pemodelanBasisData': 2, 'sistemDigital': 4, 'strukturData': 2, 'matriksDanRuangVektor': 5, 'probabilitasDanStatistikaA': 2, 'pemrogramanBerorientasiObyekA': 5, 'analisisDanPerancanganPerangkatLunak': 2, 'sistemBasisData': 4, 'desainDanAnalisisAlgoritma': 4, 'organisasiDanArsitekturKomputer': 4, 'teoriBahasaDanAutomata': 2, 'jaringanKomputer': 3, 'kecerdasanBuatan': 2, 'sistemOperasiA': 4, 'pemrogramanWeb': 3, 'implementasiDanPengujianPerangkatLunak': 2, 'dasarPemodelanDanSimulasi': 3, 'interaksiManusiaDanKomputerA': 4, 'sistemParalelDanTerdistribusi': 3, 'pembelajaranMesin': 2, 'inginMemilihTrackTaYangMana?': 'Advanced AI'}]
     if len(mks) <= 0:
         return {"error": "NIM Not Found!"}
